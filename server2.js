c01467ad7d3354bded712e3d1b0ad2b72f154c3f
raz0229 <raja.zafar229@gmail.com>
2021-08-06 21:50:28 +0500

    Added PaS

--- server2.js
+++ server2.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const forceSsl = require('force-ssl-heroku');
+const forceSsl = require('heroku-ssl-redirect');
 const app = express();
 const path = require('path');
 
 const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(forceSsl)
+app.use(forceSsl()) // enable ssl redirect
 
 app.get('*', (req, res) => {
 res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
